5358eed5ebf96cae98124be95c7bce7ff813e654
Ondřej Surý <ondrej@sury.org>
2017-12-06 14:15:43 +0000

    Get rid of extra php7.2-sodium module

--- debian/rules.d/ext-sodium.mk
+++
@@ -1,6 +0,0 @@
-ext_PACKAGES     += sodium
-sodium_DESCRIPTION := libsodium
-sodium_EXTENSIONS  := sodium
-sodium_config      := --with-sodium=shared,/usr
-export sodium_EXTENSIONS
-export sodium_DESCRIPTION
