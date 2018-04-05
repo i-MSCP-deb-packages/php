23375f8d64413c75dc3264da88b0a2758892fca7
Ondřej Surý <ondrej@sury.org>
2018-04-05 08:41:31 +0000

    New upstream version 7.1.16

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 15
+#define PHP_RELEASE_VERSION 16
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "7.1.15"
-#define PHP_VERSION_ID 70115
+#define PHP_VERSION "7.1.16"
+#define PHP_VERSION_ID 70116
